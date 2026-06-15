df3cf911fd7be7b582f31d071bd26042805803db
Adamachmad <adamachmad8@gmail.com>
2026-06-15 14:52:32 +0800

    tambah message rate limit

--- server.ts
+++ server.ts
@@ -343,6 +343,7 @@ function configureApp (app: ReturnType<typeof express>, seq: typeof sequelize) {
   app.use('/rest/user/reset-password', rateLimit({
     windowMs: 5 * 60 * 1000,
     max: 10,
+    message: 'Too many login attempts from this IP, please try again after 5 minutes',
     keyGenerator ({ headers, ip }: { headers: any, ip: any }) { return headers['X-Forwarded-For'] ?? ip } // vuln-code-snippet vuln-line resetPasswordMortyChallenge
   }))
   // vuln-code-snippet end resetPasswordMortyChallenge
